59c9cc174850ade81f333bf4f876ef940e63f97e
goisygor <ygorgoissenai@gmail.com>
2023-09-01 21:47:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const $lucesDelCirculo = document.querySelectorAll('.luces-circulo');
+let contadorDeLuz = 0;
+
+const mostrarLuz = () =>{
+    $lucesDelCirculo[contadorDeLuz].className = 'luces-circulo';
+    contadorDeLuz++;
+
+    if(contadorDeLuz > 2) contadorDeLuz = 0;
+
+    const luzActual = $lucesDelCirculo[contadorDeLuz];
+    luzActual.classList.add(luzActual.getAttribute('color'))
+
+    const botao1 = document.getElementById('botao1');
+    const botao2 = document.getElementById('botao2');
+    const botao3 = document.getElementById('botao3');
+
+    botao1.addEventListener('click',
+    function(){
+        alert('botao 1 clicado');
+    });
+    botao2.addEventListener('click',
+    function(){
+        alert('botao 1 clicado');
+    });
+    botao3.addEventListener('click',
+    function(){
+        alert('botao 1 clicado');
+    });
+
+        botao4.addEventListener('click',
+        function(){
+            alert('botao 4 clicado');
+        });
+}

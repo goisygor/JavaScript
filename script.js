9c64042929e942c209d7d3d6978332c297cc5e21
goisygor <ygorgoissenai@gmail.com>
2023-09-22 18:55:55 -0300

    pronto

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
-/* nesse codigos estamos armazenando 
-variaveis que foram declarados no html para pode utilizar elas depois no css e aqui no javascript*/
-const img = document.getElementById('img');
-const buttons = document.getElementById('buttons');
-let colorIndex = 0;
-let intervalId = null;
+// Neste código, estamos armazenando variáveis que serão usadas posteriormente no CSS.
+const img = document.getElementById('img'); // Obtém a referência da imagem do semáforo.
+const buttons = document.getElementById('buttons'); // Obtém a referência dos botões de controle.
+let colorIndex = 0; // Variável que controla o índice da cor atual no semáforo.
+let intervalId = null; // Variável para armazenar o identificador do intervalo de tempo para a troca automática de cores.
 
+// A função "trafficLight" é acionada quando um botão é clicado e interrompe a troca automática de cores.
 const trafficLight = (event) => {
-    stopAutomatic();
-    turnOn[event.target.id](); //aqui ele irá ler qual luz de semáforo que será acessa
+    stopAutomatic(); // Chama a função para interromper a troca automática de cores.
+    turnOn[event.target.id](); // Liga a cor do semáforo correspondente ao botão clicado.
 }
 
-
-// Basicamente, esses códigos fazem com que um elemento na página mude de cor em um 
-//ciclo predefinido sempre que a função changeColor é chamada.
+// A função "nextIndex" atualiza o índice da cor do semáforo para a próxima cor.
 const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
-/* a const next index e color index são responsaveis por definir a proxima cor do semáforo
-A função "changeColor" seleciona a cor correspondente 
-ao valor atual de "colorIndex" e chama a função correspondente para ligar a cor do semáforo.
-Em seguida, chama a função "nextIndex" para definir a próxima cor. */
 
-
-const changeColor = () => { // 
-    const colors = ['red', 'yellow', 'green']
+// A função "changeColor" seleciona a próxima cor do semáforo e a exibe.
+const changeColor = () => {
+    const colors = ['red', 'yellow', 'green'];
     const color = colors[colorIndex];
-    turnOn[color]();
-    nextIndex();
-}
-// Aqui é para deixar o semaforo automatico
-const stopAutomatic = () => {/*A função "stopAutomatic" interrompe a troca automática de cores do semáforo, 
-                             utilizando o identificador do intervalo de tempo armazenado em "intervalId". */
-    clearInterval(intervalId); // aqui é para deixar o semaforo automatico em 1 seg cada cor
-    
+    turnOn[color](); // Liga a cor do semáforo.
+    nextIndex(); // Atualiza o índice da próxima cor.
 }
 
-/* a variavel turnOn é um objeto que mapeia as cores do semáforo para Funções que ligam a cor 
-correspondente no semáforo */
+// A função "stopAutomatic" interrompe a troca automática de cores do semáforo, se estiver em andamento.
+const stopAutomatic = () => {
+    clearInterval(intervalId); // Interrompe o intervalo de tempo.
+}
+
+// O objeto "turnOn" mapeia as cores do semáforo para funções que ligam a cor correspondente.
 const turnOn = {
-    'red': () => img.src = './img/vermelho.png',
-    'yellow': () => img.src = './img/amarelo.png',
-    'green': () => img.src = './img/verde.png',  
-    'automatic': () => intervalId = setInterval(changeColor, 1000)
+    'red': () => img.src = './img/vermelho.png', // Ligar a cor vermelha.
+    'yellow': () => img.src = './img/amarelo.png', // Ligar a cor amarela.
+    'green': () => img.src = './img/verde.png', // Ligar a cor verde.
+    'automatic': () => intervalId = setInterval(changeColor, 1000) // Iniciar troca automática de cores.
 }
 
-buttons.addEventListener('click', trafficLight); /*  aqui adicionei um addEventListener para que me 
-permita quando eu clicar nos botões ele faça a troca de cor do semáforo ou coloque ele em automático*/
+// Adiciona um evento de clique aos botões para acionar a função "trafficLight" quando um botão for clicado.
+buttons.addEventListener('click', trafficLight);

b6a08b363d41aa3557257c4650ed3acf06fb2bad
goisygor <ygorgoissenai@gmail.com>
2023-10-10 21:55:37 -0300

    10/10/23

--- ProjetoVue/meu-projeto-vue/src/components/CatalogoDeLivro.vue
+++ ProjetoVue/meu-projeto-vue/src/components/CatalogoDeLivro.vue
@@ -1,50 +1,93 @@
 <template>
-    <div class="catalogo-Livros">
+    <div class="catalogo-livros">
         <h2>Catálogo de Livros</h2>
         <input v-model="novoLivro" @keyup.enter="adicionarLivro" placeholder="Digite um novo Livro" />
-        <ul>
-            <li v-for="(livros, index) in livros" :key="index">
-                {{ livros.titulo }}
-                <button @click="alternarStatus(livros)">Marcar como {{
-                    livros.assistido ? 'Não Assistido' : 'Assistido' }}</button>
+        <button @click="adicionarLivro" class="adicionar-button">Adicionar Livro</button>
+        <ul class="livros-list">
+            <li v-for="(livro, index) in livros" :key="index" class="livro-item">
+                <div>
+                    <span>{{ livro.titulo }}</span>
+                    <button @click="alternarStatus(livro)" class="status-button">
+                        {{ livro.lidos ? 'Não Lido' : 'Lido' }}
+                    </button>
+                </div>
+                <button @click="removerLivro(index)" class="excluir-button">Excluir</button>
+                <button @click="marcarComoLido(index)" class="lido-button" :class="{ 'lido': livro.lidos, 'nao-lido': !livro.lidos }">
+                    Lido
+                </button>
             </li>
         </ul>
     </div>
 </template>
-    
+
 <script>
 export default {
     data() {
         return {
-            novolivros: "",
+            novoLivro: "",
             livros: [],
         };
     },
     methods: {
-        adicionarlivros() {
-            if (this.novolivros.trim() !== "") {
-                this.livros.push({ titulo: this.novolivros, lidos: false });
-                this.novolivros = "";
+        adicionarLivro() {
+            if (this.novoLivro.trim() !== "") {
+                this.livros.push({ titulo: this.novoLivro, lidos: false });
+                this.novoLivro = "";
             }
         },
-        alternarStatus(livros) {
-            livros.lidos = !livros.lidos.trim();
+        alternarStatus(livro) {
+            livro.lidos = !livro.lidos;
+        },
+        removerLivro(index) {
+            this.livros.splice(index, 1);
+        },
+        marcarComoLido(index) {
+            this.livros[index].lidos = true;
         },
     },
 };
 </script>
-    
+
 <style scoped>
 .catalogo-livros {
     margin: 20px;
 }
 
-ul {
+.livros-list {
     list-style-type: none;
     padding: 0;
 }
 
-li {
+.livro-item {
     margin: 10px 0;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.adicionar-button, .status-button, .excluir-button, .lido-button {
+    background-color: #007BFF;
+    color: #fff;
+    padding: 5px 10px;
+    border: none;
+    cursor: pointer;
+    margin: 5px;
+    border-radius: 5px;
+    transition: background-color 0.3s;
+}
+
+.adicionar-button:hover, .status-button:hover, .excluir-button:hover, .lido-button:hover {
+    background-color: #0056b3;
+}
+
+.lido {
+    background-color: #4CAF50; /* Verde */
+}
+
+.nao-lido {
+    background-color: #FF5722; /* Laranja */
 }
 </style>
